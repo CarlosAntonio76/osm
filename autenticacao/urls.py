f4955f010d634e48c1c7212bc475132be285b786
cpd03 <cpd03@avscom.local>
2024-06-06 18:23:30 -0300

    Iniciando cadastro de usuarios e login

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,7 +3,7 @@ from . import views as v
 
 urlpatterns = [
     path('cadastro/', v.cadastro, name='cadastro'),
-    path('login', v.login, name='login'),
+    path('login/', v.login, name='login'),
     path('ativar_conta/<str:token>/', v.ativar_conta, name='ativar_conta'),
     path('sair/', v.sair, name='sair'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from . utils import password_is_valid, email_html
+from . utils import password_is_valid, email_html
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
@@ -8,7 +8,7 @@ from django.contrib import messages
 from django.contrib import auth
 import os
 from django.conf import settings
-#from .models import Ativacao
+from .models import Ativacao
 from hashlib import sha256
 
 
@@ -19,18 +19,72 @@ def cadastro(request):
             return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('usuario')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
 
         if User.objects.filter(username=username):
             messages.add_message(request, constants.WARNING, f"Usuário {username}, já existe!")
-            return redirect('/auth/cadastro')
+            return redirect('autenticacao/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+            ativacao.save()
+
+            path_template = os.path.join(settings.BASE_DIR, 'autenticacao/templates/emails/cadastro_confirmado.html')
+            email_html(path_template, 'Cadastro confirmado', [email,], username=username, link_ativacao=f"http://127.0.0.1:8000/auth/ativar_conta/{token}")
+
+            messages.add_message(request, constants.SUCCESS, "Usuário salvo com sucesso!")
+            return redirect('autenticacao/cadastro')
+            
+        except Exception as e:
+            messages.add_message(request, constants.ERROR, "Erro interno do sistema")
+            print('Este é o erro: ', e)
+            return redirect('autenticacao/cadastro')
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING, 'Esse token já foi usado')
+        return redirect('/auth/cadastro')
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta ativa com sucesso')
+    return redirect('/auth/cadastro') #correto é /auth/logar
+
 
 
 def login(request):
-    ...
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/autenticacao/login/')
+        else:
+            auth.login(request, usuario)
+            messages.add_message(request, constants.ERROR, 'Teste Logado com sucesso!')
+            return redirect('/autenticacao/cadastro/')
+ 
 
 def ativacao(request):
     ...
@@ -39,4 +93,5 @@ def ativar_conta(request):
     ...
 
 def sair(request):
-    ...
+    auth.logout(request)
+    return redirect('/autenticacao/logar')
